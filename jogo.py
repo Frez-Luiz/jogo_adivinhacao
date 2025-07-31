dea5bea72a03a1de1ed4f046c4c92e3c3b5acfc0
Luiz Felipe Frez <luiz.frez.santos@escola.pr.gov.br>
2025-07-31 14:50:16 +0000

    parte 2 do jogo

--- jogo.py
+++ jogo.py
@@ -10,3 +10,15 @@ print ("_____________________________________")
 numeroSecreto = random.randrange(0,100)
 totalTentativas = 0
 pontos = 100
+
+print("     Qual níveis de dificuldade?  ")
+print(" (1)- Fácil (2)- Médio (3)- Difícil")
+
+nivel = int(input("Escolha um nível:  "))
+
+if(nivel == 1):
+    print("20 tentativas! Ta com medo, frango 🤣")
+elif(nivel == 2):
+    print(" 10 tentativas! Ta com pouca coragem 🤣")
+elif(nivel == 3):
+    print(" 5 tentativas! Ta corajoso demais pro meu gosto 😱 ")
