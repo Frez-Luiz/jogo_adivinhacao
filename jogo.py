e7a4451cff872c49e7e2a97d01c5b11f77064d84
Luiz Felipe Frez <luiz.frez.santos@escola.pr.gov.br>
2025-07-31 12:17:34 +0000

    jogo adivinhação

---
+++ jogo.py
@@ -0,0 +1,12 @@
+
+import random
+
+print ("_____________________________________\n")
+print ("///    Jogo de adivinhação       ///")
+print ("//         Luiz Felipe            //")
+print ("///   Bem vindo ao meu jogo      ///")
+print ("_____________________________________")
+
+numeroSecreto = random.randrange(0,100)
+totalTentativas = 0
+pontos = 100
